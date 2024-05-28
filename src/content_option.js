6ca2cc3357a05f8e9af460b9ea44594c1ccd301b
Jorge162017 <lop221038@uvg.edu.gt>
2024-05-27 19:43:17 -0600

    Portafolio finalizado

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "JORGE";
+const meta = {
+    title: "Jorge Lopez",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "Yo soy Jorge Lopez",
+    animated: {
+        first: "Estudiante de ingeniería de software",
+        second: "Desarrollador web",
+        third: "Desarrollador movil",
+    },
+    description: "Apasionado por la tecnología y el desarrollo de software, siempre busco aprender y mejorar mis habilidades.",
+    your_img_url: "https://fv5-4.failiem.lv/thumb_show.php?i=ged68s3w9w&view&v=1",
+};
+
+const dataabout = {
+    title: "Acerca de mi",
+    aboutme: "Soy un estudiante de ingeniería con experiencia en desarrollo web. Me enfoco en crear soluciones efectivas y centradas en la experiencia del usuario utilizando diversas tecnologías y herramientas.",
+};
+const worktimeline = [{
+        jobtitle: "Graduado en high school",
+        where: "New Westminster High School",
+        date: "2020",
+    },
+    {
+        jobtitle: "Inicio de la carrera de ingeniería de software",
+        where: "Universidad del Valle de Guatemala",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 75,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Node.js",
+        value: 70,
+    },
+    {
+        name: "MySQL",
+        value: 85,
+    },
+    {
+        name: "Kotlin",
+        value: 90,
+    },
+    {
+        name: "Postman",
+        value: 90,
+    },
+
+
+];
+
+const services = [{
+        title: "Desarrollo Web",
+        description: "Ofrezco servicios de desarrollo web, creando sitios y aplicaciones web modernos, responsivos y de alto rendimiento. Me especializo en diseñar y desarrollar soluciones personalizadas que se adaptan a las necesidades de tu negocio, asegurando una experiencia de usuario excepcional en cualquier dispositivo. ",
+    },
+    {
+        title: "Desarrollo Móvil",
+        description: "Ofrezco servicios de desarrollo de aplicaciones móviles para Android y iOS. Me especializo en crear aplicaciones intuitivas, funcionales y atractivas que proporcionan una excelente experiencia de usuario en ambas plataformas.",
+    },
+    {
+        title: "Diseño de UI & UX",
+        description: "Ofrezco servicios de diseño de interfaces de usuario (UI) y experiencia de usuario (UX). Con un enfoque en la usabilidad y la estética, creo diseños intuitivos y visualmente atractivos que mejoran la interacción del usuario y potencian la experiencia general con tus productos digitales.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://fv5-3.failiem.lv/thumb_show.php?i=2n8ucstz5x&view&v=1",
+        description: "Diseño de una app movil para rutinas de ejercicio",
+        link: "https://www.behance.net/gallery/198869493/OnShape",
+    },
+    {
+        img: "https://fv5-2.failiem.lv/thumb_show.php?i=mtca4u82fs&view&v=1",
+        description: "Diseno de una pagina web como steam pero para mamas",
+        link: "https://www.behance.net/gallery/197058507/Steamom",
+    },
+    {
+        img: "https://fv5-4.failiem.lv/thumb_show.php?i=65vv5abpqy&view&v=1",
+        description: "Propuesta de integracion de funcion de prestamos en whatsapp",
+        link: "https://www.behance.net/gallery/193567713/WhatsApp-MoneyWay",
+    },
+    {
+        img: "https://fv5-2.failiem.lv/thumb_show.php?i=dzwxfgqhq2&view&v=1",
+        description: "Rediseño de tiktok ",
+        link: "https://www.behance.net/gallery/191085369/TIKTOK-UX-Redesign",
+    },
+    {
+        img: "https://fv5-4.failiem.lv/thumb_show.php?i=xfhr8sc7ac&view&v=1",
+        description: "Calculadora con next.js",
+        link: "https://calculadora-test.vercel.app/",
+    },
+    {
+        img: "https://fv5-2.failiem.lv/thumb_show.php?i=6ypcfmky8h&view&v=1",
+        description: "Blog de viajes con API propia",
+        link: "https://blog-viajes-tawny.vercel.app/",
+    },
+
+    {
+        img: "https://fv5-2.failiem.lv/thumb_show.php?i=cmwhegf7up&view&v=1",
+        description: "Aplicacion movil de gestor de torneos de futbol",
+        link: "https://github.com/Jorge162017/ProyectoX3",
+    },
+    {
+        img: "https://fv5-3.failiem.lv/thumb_show.php?i=gd6zyzm9wv&view&v=1",
+        description: "Chat con javascript y una API",
+        link: "http://3.129.191.211/Lab5WEB_JS/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Jorge62175@gmail.com",
+    YOUR_FONE: "+502 4815-3977",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_w5idtrj",
+    YOUR_TEMPLATE_ID: "template_lpu8sls",
+    YOUR_USER_ID: "IFXpSfbG5d1HFKCgH",
+};
+
+const socialprofils = {
+    github: "https://github.com/Jorge162017",
+    facebook: "https://www.facebook.com/sam.flin.92/",
+    linkedin: "https://www.linkedin.com/in/jorge-lopez-a39270310/",
+    instagram: "https://www.instagram.com/jorgelopezzzz__/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
